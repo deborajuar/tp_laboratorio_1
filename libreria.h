50e5fda61573935e069c347d70d9fd5f7514a6a8
Juarez Paiva Debora <40776559+deborajuar@users.noreply.github.com>
2020-04-18 18:28:53 -0300

    Add files via upload

---
+++ libreria.h
@@ -0,0 +1,10 @@
+
+#ifndef _LIBRERIA_H
+#define _LIBRERIA_H
+float suma (float, float);
+float resta(float, float);
+float multiplicacion (float, float);
+float division (float, float);
+float factorial (float);
+float factorial_2(float);
+#endif //
